570baa6f682caa1a0d482a16f7e5af36c8f7633a
reiji matsumura <reiji0120124@gmail.com>
2020-01-09 00:00:05 +0900

    カード作成

--- flutter_app/lib/SecondPage.dart
+++ flutter_app/lib/SecondPage.dart
@@ -2,15 +2,52 @@ import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
   Widget build(BuildContext context) {
+
+    final Size size = MediaQuery.of(context).size;
+
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(title: const Text('second Page')),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back'),
+        child: ListView(
+          // This next line does the trick.
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Card(
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {
+                  print('Card tapped.');
+                },
+                child: Container(
+                  height: 300,
+                  width: 300,
+                ),
+              ),
+              margin: EdgeInsets.only(
+                top: 300,
+                bottom: 150,
+                left: 50,
+                right: 50,
+              ),
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.green,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.orange,
+            ),
+          ],
         ),
       ),
     );
