1d9039aa0f496f3d2d389d7c089b71ebcf60b107
reiji matsumura <reiji0120124@gmail.com>
2020-01-09 00:23:31 +0900

    複数のカードを表示するように

--- flutter_app/lib/SecondPage.dart
+++ flutter_app/lib/SecondPage.dart
@@ -4,49 +4,46 @@ class SecondPage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     final Size size = MediaQuery.of(context).size;
+    var list = ["one", "two", "three", "four"];
 
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(title: const Text('second Page')),
-      body: Center(
+      body: Container(
+        color: Colors.blue,
         child: ListView(
           // This next line does the trick.
           scrollDirection: Axis.horizontal,
           children: <Widget>[
-            Card(
+          for(var item in list ) Container (
+            margin: EdgeInsets.only(
+              top: 300,
+              bottom: 150,
+              left: 40,
+              right: 40,
+            ),
+            child:  Card(
               child: InkWell(
                 splashColor: Colors.blue.withAlpha(30),
                 onTap: () {
                   print('Card tapped.');
                 },
                 child: Container(
-                  height: 300,
-                  width: 300,
+                    height: 300,
+                    width: 300,
+                    child: Center(
+                      child: Text(
+                          item
+                      ),
+                    ),
                 ),
               ),
-              margin: EdgeInsets.only(
-                top: 300,
-                bottom: 150,
-                left: 50,
-                right: 50,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
               ),
+              elevation:10,
             ),
-            Container(
-              width: 160.0,
-              color: Colors.blue,
-            ),
-            Container(
-              width: 160.0,
-              color: Colors.green,
-            ),
-            Container(
-              width: 160.0,
-              color: Colors.yellow,
-            ),
-            Container(
-              width: 160.0,
-              color: Colors.orange,
-            ),
+          ),
           ],
         ),
       ),
