25b74ce00ed1df7c6a92f775488dcf91f4ec0fe1
reiji matsumura <reiji0120124@gmail.com>
2020-01-09 18:51:52 +0900

    カードを別部品としてかくり

--- flutter_app/lib/SecondPage.dart
+++ flutter_app/lib/SecondPage.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
+  var list = ["one", "two", "three", "four"];
+
   Widget build(BuildContext context) {
 
     final Size size = MediaQuery.of(context).size;
-    var list = ["one", "two", "three", "four"];
 
     // TODO: implement build
     return Scaffold(
@@ -17,35 +18,42 @@ class SecondPage extends StatelessWidget {
           children: <Widget>[
           for(var item in list ) Container (
             margin: EdgeInsets.only(
-              top: 300,
-              bottom: 150,
-              left: 40,
-              right: 40,
             ),
-            child:  Card(
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                  print('Card tapped.');
-                },
-                child: Container(
-                    height: 300,
-                    width: 300,
-                    child: Center(
-                      child: Text(
-                          item
-                      ),
-                    ),
-                ),
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              elevation:10,
-            ),
+            child: _customCard(item, size)
           ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _customCard(String item, Size size) {
+    return Container (
+      width: size.width,
+      margin: EdgeInsets.only(
+        top: 50,
+        left: 100,
+        right: 100,
+        bottom: 50
+      ),
+      child: Card(
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            print('Card tapped.');
+          },
+          child: Container(
+            child: Center(
+              child: Text(
+                item,
+              ),
+            ),
+          ),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        elevation:10,
       ),
     );
   }
