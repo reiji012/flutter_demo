d45ecbe2d712552ddbc4a2c412874cc9dce8851e
reiji matsumura <reiji0120124@gmail.com>
2020-02-06 15:27:49 +0900

    カードビューの整形

--- flutter_app/lib/SecondPage.dart
+++ flutter_app/lib/SecondPage.dart
@@ -1,27 +1,58 @@
+
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
   var list = ["one", "two", "three", "four"];
 
+  double cardMarginLeft(String item) {
+    double marginLeft = 0;
+    if (item == "one") {
+      marginLeft = 30;
+    }
+    return marginLeft;
+  }
+
+  double cardMarginRight(String item) {
+    double marginRight = 0;
+    if (item == "four") {
+      marginRight = 30;
+    }
+    return marginRight;
+  }
+
   Widget build(BuildContext context) {
 
     final Size size = MediaQuery.of(context).size;
+    double cardHeight = size.height / 2;
+    double cardWidth = size.width - 50;
 
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(title: const Text('second Page')),
       body: Container(
-        color: Colors.blue,
-        child: ListView(
-          // This next line does the trick.
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-          for(var item in list ) Container (
-            margin: EdgeInsets.only(
-            ),
-            child: _customCard(item, size)
-          ),
-          ],
+          color: Colors.blue,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: cardHeight / 2,
+              ),
+              Container(
+                height: cardHeight,
+                child: ListView(
+
+                  // This next line does the trick.
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    for(var item in list ) Container (
+                        height: cardHeight,
+                        width: cardWidth,
+                        child: _customCard(item, size)
+                    ),
+                  ],
+                ),
+              ),
+            ],
         ),
       ),
     );
@@ -31,10 +62,10 @@ class SecondPage extends StatelessWidget {
     return Container (
       width: size.width,
       margin: EdgeInsets.only(
-        top: 50,
-        left: 100,
-        right: 100,
-        bottom: 50
+        top: 10,
+        left: cardMarginLeft(item),
+        right: cardMarginRight(item),
+        bottom: 50,
       ),
       child: Card(
         child: InkWell(
